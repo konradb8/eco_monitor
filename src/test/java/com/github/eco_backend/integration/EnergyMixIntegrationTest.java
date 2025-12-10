3d15fe074fac1247a5f634ed1a2cfbf0c52085aa
konradb8 <konradbugaj547@gmail.com>
2025-12-10 20:53:17 +0100

    test: add integration tests

---
+++ src/test/java/com/github/eco_backend/integration/EnergyMixIntegrationTest.java
@@ -0,0 +1,79 @@
+package com.github.eco_backend.integration;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.hamcrest.Matchers.notNullValue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EnergyMixIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeAll
+     static void setUrl() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.basePath = "/api/v1/";
+    }
+
+    @BeforeEach
+    void setPort() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    @DisplayName("")
+    void getMixForecast_thenReturnBodyAndStatusOK() {
+        RestAssured.given()
+                .when()
+                .get("mix-forecast")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .body("$", hasSize(3));
+
+    }
+
+    @Test
+    @DisplayName("")
+    void getChargeWindow_thenReturnBodyAndStatusOK() {
+        RestAssured.given()
+                .param("duration", 3)
+                .when()
+                .get("charge-window")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .body("from", is(notNullValue()))
+                .body("to", is(notNullValue()))
+                .body("cleanEnergyPerc", is(notNullValue()));
+    }
+
+    @Test
+    @DisplayName("")
+    void getChargeWindow_DurationInvalid_thenReturnBodyAndStatusBadReqeust() {
+        RestAssured.given()
+                .param("duration", 35)
+                .when()
+                .get("charge-window")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .contentType(ContentType.JSON)
+                .body("timestamp", matchesPattern("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}"))
+                .body("status", is(HttpStatus.BAD_REQUEST.value()))
+                .body("errorMessage", is("BAD_REQUEST"))
+                .body("message", is("Duration must be between 1 and 6 hours"))
+                .body("path", is("/api/v1/charge-window"));
+    }
+}
